57a3752e404874a424cc12f6ea40045602709907
NaughtCrab <xinwei.mao@mailbox.tu-dresden.de>
2019-12-20 01:13:26 +0100

    version3

--- client.c
+++ client.c
@@ -7,9 +7,171 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <libgen.h>	//basename
+#include <sys/time.h>
 
 #include "dccp.h"
-#define BUFFER_SIZE 4096
+#include "common.h"
+
+#define BUFFER_SIZE 1400
+#define NAME_SIZE 512
+
+struct tfrc_tx_info {
+	uint64_t tfrctx_x;
+	uint64_t tfrctx_x_recv;
+	uint32_t tfrctx_x_calc;
+	uint32_t tfrctx_rtt;
+	uint32_t tfrctx_p;
+	uint32_t tfrctx_rto;
+	uint32_t tfrctx_ipi;
+};
+
+int error_exit(const char *str);
+
+void sendfile(FILE *fp, FILE *data1, FILE *data2, int socket_fd, int socket_fd_2);
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4) {
+		printf("Usage: ./client <server address1> <server address2> <file name> \n");
+		exit(-1);
+	}
+
+
+	/************************/
+	/*        Part I        */
+	/************************/
+
+	// type of socket 1 created
+	struct sockaddr_in server_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+	};
+	
+	// type of socket 2 created
+	struct sockaddr_in server_addr_2 = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+	};
+
+	
+	/* socket 1 configuration */
+	if (!inet_pton(AF_INET, argv[1], &server_addr.sin_addr.s_addr)) {
+		printf("Invalid address %s\n", argv[1]);
+		exit(-1);
+	}
+	// create socket 1 
+	int socket_fd = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP);
+	if (socket_fd < 0)
+		error_exit("socket");
+
+	if (setsockopt(socket_fd, SOL_DCCP, DCCP_SOCKOPT_SERVICE, &(int){htonl(SERVICE_CODE)}, sizeof(int)))
+		error_exit("setsockopt(DCCP_SOCKOPT_SERVICE)");
+
+	if (setsockopt(socket_fd, SOL_DCCP, DCCP_SOCKOPT_CCID, &(uint8_t){3}, sizeof(uint8_t)))
+		error_exit("setsockopt(DCCP_SOCKOPT_CCID)");
+	
+	// set socket 1 to allow multiple connection
+	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int)))
+		error_exit("setsockopt(SO_REUSEADDR)");
+    
+	// connect socket 1
+	if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+		error_exit("connect1");
+
+
+	/* socket 2 configuration */	
+	if (!inet_pton(AF_INET, argv[2], &server_addr_2.sin_addr.s_addr)) {
+		printf("Invalid address %s\n", argv[2]);
+		exit(-1);
+	}
+	
+	// create socket 2
+	int socket_fd_2 = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP);
+	if (socket_fd_2 < 0)
+		error_exit("socket");
+
+	if (setsockopt(socket_fd_2, SOL_DCCP, DCCP_SOCKOPT_SERVICE, &(int){htonl(SERVICE_CODE)}, sizeof(int)))
+		error_exit("setsockopt(DCCP_SOCKOPT_SERVICE)");
+
+	if (setsockopt(socket_fd_2, SOL_DCCP, DCCP_SOCKOPT_CCID, &(uint8_t){3}, sizeof(uint8_t)))
+		error_exit("setsockopt(DCCP_SOCKOPT_CCID)");
+	
+	// set socket 2 to allow multiple connection
+	if (setsockopt(socket_fd_2, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int)))
+		error_exit("setsockopt(SO_REUSEADDR)");
+	
+	// connect socket 2
+	if (connect(socket_fd_2, (struct sockaddr *)&server_addr_2, sizeof(server_addr)))
+		error_exit("connect2");	
+
+
+	/************************/
+	/*       Part II        */
+	/************************/
+
+	/* Create file to save data */
+	FILE *data1 = fopen("data1.txt", "w+");
+	if(data1 == NULL) {
+		error_exit("Failed to create data1.txt");
+	}
+
+	FILE *data2 = fopen("data2.txt", "w+");
+	if(data2 == NULL) {
+		error_exit("Failed to create data2.txt");
+	}
+	/* Start file transfer */
+	// get the file name and address
+	char *filename = basename(argv[3]); 
+	if(filename == NULL) {
+		error_exit("File is not exist");
+	}
+	printf("The File Name is: %s\n", filename);
+
+	/* send file name
+
+	char file_name[NAME_SIZE];
+	bzero(file_name, NAME_SIZE);
+	strncpy(file_name, filename, strlen(filename));
+	if(send(socket_fd, file_name, NAME_SIZE, 0) < 0) {
+		error_exit("Failed to send file name");
+	}
+	*/
+	
+	// Open the file 
+	FILE *fp = fopen(argv[3], "rb");
+	if(fp == NULL) {
+		error_exit("FOPEN");
+	}
+
+	// begin to send the file 
+	struct timeval tp1, tp2;
+	int time_start, time_end;
+	gettimeofday(&tp1, NULL);
+	time_start = tp1.tv_sec * 1000 + tp1.tv_usec/1000;
+
+	sendfile(fp, data1, data2, socket_fd, socket_fd_2);
+	puts("Send successfully");
+	
+	/* closing */
+	gettimeofday(&tp2, NULL);
+	time_end = tp2.tv_sec * 1000 + tp2.tv_usec/1000;
+	printf("Start time is %d, End time is %d\n", time_start, time_end);
+	printf("The total run time is: %d\n", (time_end - time_start));
+
+	// close the transfer file
+	fclose(fp);
+
+	// close the data file 
+	fclose(data1);
+	fclose(data2);
+
+	// close the socket 1 and 2
+	close(socket_fd);
+	close(socket_fd_2);
+	return 0;
+}
+
 
 int error_exit(const char *str)
 {
@@ -17,84 +179,98 @@ int error_exit(const char *str)
 	exit(errno);
 }
 
-void sendfile(FILE *fp, int socket_fd);
+void sendfile(FILE *fp, FILE *data1, FILE *data2, int socket_fd, int socket_fd_2) {
+	struct tfrc_tx_info tx_info;
+	unsigned int tx_info_size;
 
-int main(int argc, char *argv[])
-{
-	if (argc < 5) {
-		printf("Usage: ./client <server address> <port> <service code> <file name> \n");
-		exit(-1);
-	}
-	struct sockaddr_in server_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(atoi(argv[2])),
-	};
+	int n = 0;		// data size each time
+	int total = 0;	// total data size 
+	int count = 0;	// choose destination
+	int ret1, ret2;
 
-	if (!inet_pton(AF_INET, argv[1], &server_addr.sin_addr.s_addr)) {
-		printf("Invalid address %s\n", argv[1]);
-		exit(-1);
-	}
+	char sendline[BUFFER_SIZE];		// buffer to store data
+	bzero(sendline, BUFFER_SIZE);	// clear the buffer
 
-	int socket_fd = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP);
-	if (socket_fd < 0)
-		error_exit("socket");
+	tx_info_size = sizeof(tx_info);
 
-	if (setsockopt(socket_fd, SOL_DCCP, DCCP_SOCKOPT_SERVICE, &(int){htonl(atoi(argv[3]))},
-								 sizeof(int)))
-		error_exit("setsockopt(DCCP_SOCKOPT_SERVICE)");
-
-	if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
-		error_exit("connect");
-	/*
-	// Get the maximum packet size
-	uint32_t mps;
-	socklen_t res_len = sizeof(mps);
-	if (getsockopt(socket_fd, SOL_DCCP, DCCP_SOCKOPT_GET_CUR_MPS, &mps, &res_len))
-		error_exit("getsockopt(DCCP_SOCKOPT_GET_CUR_MPS)");
-	printf("Maximum Packet Size: %d\n", mps);
-	*/
-
-	// Get the file name
-	char *filename = basename(argv[4]);
-	if(filename == NULL) {
-		error_exit("File is not exist");
-	}
-	printf("The File Name is: %s\n", filename);
-
-	// Send the file name (size of name should be same as buffer size)
-	char buffer[BUFFER_SIZE];
-	bzero(buffer, BUFFER_SIZE);
-	strncpy(buffer, filename, strlen(filename));
-	if(send(socket_fd, buffer, BUFFER_SIZE, 0) < 0) {
-		error_exit("Failed to send file name");
-	}
+	// time stamp of socket 1 and 2
+	struct timeval tp1, tp2;
+	// time variables of socket 1
+	int s1_tx1 = 0;
+	int s1_tx2 = 0;
 	
-	// Open the file to be sen
-	FILE *fp = fopen(argv[4], "rb");
-	if(fp == NULL) {
-		error_exit("Failed to open file");
-	}
+	// time variables of socket 2
+	int s2_tx1 = 0;
+	int s2_tx2 = 0;
 
-	// read and send the file
-	sendfile(fp, socket_fd);
-	puts("Send successfully");
-		
-	// Close the file
-	fclose(fp);
+	while((n = fread(sendline, sizeof(char), BUFFER_SIZE, fp)) > 0) {
+		while (1) {
+			count += 1;
+			if (count % 2 != 0) {
+				ret1 = send(socket_fd, sendline, n, 0);		
+				if (ret1 < 0) {
+					if (errno == EAGAIN) {
+						usleep(3000);
+						continue;
+					}
+					printf("ret %d\n", ret1);
+					perror("Socket1: Failed to send file");
+					exit(1);
+				} else {
+					break;
+				}
 
-	close(socket_fd);
-	return 0;
-}
-
-void sendfile(FILE *fp, int socket_fd) {
-	int n = 0;
-	char sendline[BUFFER_SIZE];
-	bzero(sendline, BUFFER_SIZE);
-	while(n = fread(sendline, sizeof(char), BUFFER_SIZE, fp) > 0) {
-		if (send(socket_fd, sendline, n, 0) < 0) {
-			perror("Failed to send file");
-			exit(0);
+			
+			} else {
+				ret2 = send(socket_fd_2, sendline, n, 0);		
+				if (ret2 < 0) {
+					if (errno == EAGAIN) {
+						usleep(3000);	//set retry time, not sure???
+						continue;
+					}
+					perror("Socket2: Failed to send file");
+					exit(1);
+				} else {
+					break;
+				}
+			}
 		}
-		bzero(sendline, BUFFER_SIZE); // clear the send buffer
+		
+		/* get the data of socket 1 */
+		if (getsockopt(socket_fd, SOL_DCCP, DCCP_SOCKOPT_CCID_TX_INFO, &tx_info, &tx_info_size))
+			perror("getsockopt(DCCP_SOCKOPT_CCID_TX_INFO)");
+
+		s1_tx1 = tx_info.tfrctx_x;
+		// get time stamp for each transfer in socket 1
+		if (s1_tx1 != s1_tx2) {
+			s1_tx2 = s1_tx1; 
+			gettimeofday(&tp1, NULL);			
+			fprintf(data1, "%ld ", (tp1.tv_sec % 1000) * 1000000 + tp1.tv_usec);
+			fprintf(data1, "%lu\n", tx_info.tfrctx_x);
+		}
+		fprintf(stderr, "Socket1 rate: %lu rtt: %u loss: %u\n", tx_info.tfrctx_x, tx_info.tfrctx_rtt, tx_info.tfrctx_p);
+		
+		/* get the data of socket 2 */
+		if (getsockopt(socket_fd_2, SOL_DCCP, DCCP_SOCKOPT_CCID_TX_INFO, &tx_info, &tx_info_size))
+			perror("getsockopt(DCCP_SOCKOPT_CCID_TX_INFO)");
+
+		s2_tx1 = tx_info.tfrctx_x;
+		// get time stamp for each transfer in socket 2
+		if (s2_tx1 != s2_tx2) {
+			s2_tx2 = s2_tx1;
+			gettimeofday(&tp2, NULL);
+			fprintf(data2, "%ld ", (tp2.tv_sec % 1000) * 1000000 + tp2.tv_usec);
+			fprintf(data2, "%lu\n", tx_info.tfrctx_x);
+		}
+		fprintf(stderr, "Socket2 rate: %lu rtt: %u loss: %u\n", tx_info.tfrctx_x, tx_info.tfrctx_rtt, tx_info.tfrctx_p);
+		
+		total += n;
+		fprintf(stderr, "total: %d\n", total);
+
+		usleep(1);
+		// clear the buffer for next transfer
+		bzero(sendline, BUFFER_SIZE); 
 	}
 }
+
+
--- server.c
+++ server.c
@@ -6,14 +6,15 @@
 #include <netinet/in.h> // sin_addr
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #include "dccp.h"
+#include "common.h"
 
-#define PORT 1337
-#define SERVICE_CODE 42
-#define BUFFER_SIZE 512
 
-void writefile(int sock_fd, FILE *fp);
+#define BUFFER_SIZE 2800
+
+int writefile(int sock_fd, FILE *fp);
 
 int error_exit(const char *str)
 {
@@ -24,46 +25,66 @@ int error_exit(const char *str)
 int main(int argc, char **argv)
 {	
 
-	/* ========== Part I ========= */
+	/************************/
+	/*        Part I        */
+	/************************/
 
-	// Create socket and return value
-	int listen_sock = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP);	// AF_INET is IPV4 address, IPPROTO_DCCP is dccp protocol
+	// Create a master socket
+	int listen_sock = socket(AF_INET, SOCK_DCCP, IPPROTO_DCCP);	
 	if (listen_sock < 0)
 		error_exit("socket");
 	
-	// 1. Create sockaddr_in structure
+	// type of socket created
 	struct sockaddr_in servaddr = {
-		.sin_family = AF_INET,	//Address Familiy, also is Address type like IPV4 and IPV6
-		.sin_addr.s_addr = htonl(INADDR_ANY),	// 32 bits IP address
-		.sin_port = htons(PORT),	// 16 bits port address
+		.sin_family = AF_INET,	
+		.sin_addr.s_addr = htonl(INADDR_ANY),	
+		.sin_port = htons(PORT),	
 	};
-
-	//?
-	if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &(int) {1}, sizeof(int)))
+	
+	// set master socket to allow multiple connections
+	if (setsockopt(listen_sock, SOL_DCCP, SO_REUSEADDR, &(int) {1}, sizeof(int)))
 		error_exit("setsockopt(SO_REUSEADDR)");
 	
-	// 2. bind socket with IP, port 
+	// bind socket with IP, port 
 	if (bind(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)))
 		error_exit("bind");
 
 	// DCCP mandates the use of a 'Service Code' in addition the port
-	if (setsockopt(listen_sock, SOL_DCCP, DCCP_SOCKOPT_SERVICE, &(int) {
-		       htonl(SERVICE_CODE)}, sizeof(int)))
+	if (setsockopt(listen_sock, SOL_DCCP, DCCP_SOCKOPT_SERVICE, &(int) {htonl(SERVICE_CODE)}, sizeof(int)))
 		error_exit("setsockopt(DCCP_SOCKOPT_SERVICE)");
 	
-	// 3. Using listen() to put socket into passive listening state, until client wake it up
+	// Using listen() to put socket into passive listening state, until client wake it up
 	if (listen(listen_sock, 1))
 		error_exit("listen");
 
 
+	/************************/
+	/*       Part II        */
+	/************************/
 
-	/* ========== Part II ==========*/
+	
+	/* create the file to write in */
+	FILE *fp = fopen("out.bin", "wb");
+	if (fp == NULL) {
+		error_exit("Cannot open the file");
+	}
+	
+	/* create selet set */	
+	fd_set readset, servset;
+	struct timeval timeout = {0, 0};
+	int maxfdp, result;
+
+	FD_SET(listen_sock, &servset);
 	for (;;) {
 
 		printf("Waiting for connection...\n");
-
+		
+		/* create two client socket */
 		struct sockaddr_in client_addr;
+		struct sockaddr_in client_addr_2;
+
 		socklen_t addr_len = sizeof(client_addr);
+		socklen_t addr_len_2 = sizeof(client_addr_2);
 
 		int conn_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &addr_len);
 		if (conn_sock < 0) {
@@ -71,59 +92,84 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		printf("Connection received from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		int conn_sock_2 = accept(listen_sock, (struct sockaddr *)&client_addr_2, &addr_len_2);
+		if (conn_sock_2 < 0) {
+			perror("accept");
+			continue;
+		}
+
+		printf("Connection1 received from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		printf("Connection2 received from %s:%d\n", inet_ntoa(client_addr_2.sin_addr), ntohs(client_addr_2.sin_port));
 		
-		//Get the file name
-		char filename[BUFFER_SIZE]; // file name 
-		bzero(filename, BUFFER_SIZE); // set all are zero
+		/* select maxfdp setting, usually max(socketfd)+1 */
+		maxfdp = conn_sock > conn_sock_2 ? conn_sock + 1 : conn_sock_2 + 1;
+		printf("conn_sock is:%d, conn_sock_2 is: %d, listen_sock is: %d, maxfdp is: %d\n", conn_sock, conn_sock_2, listen_sock, maxfdp);
+		
+		while (1) {
+			/* selet parameters setting*/
+			FD_ZERO(&readset);
+			FD_SET(conn_sock, &readset);
+			FD_SET(conn_sock_2, &readset);
+			timeout.tv_sec = 1;
+			timeout.tv_usec = 0;
+			result = select(maxfdp, &readset, NULL, NULL, &timeout);
 
-		int ret = recv(conn_sock, filename, BUFFER_SIZE, 0);
-		if( ret < 0) {
-			error_exit("Cannot receive the file name");
+			if (result < 0) {	// don't find active connection
+				perror("select error");
+				break;
+			} 
+			else if (result == 0) {	// time is out
+				printf("time out \n");
+				continue;
+			}
+
+			/* Chosen socket is active*/
+			if (FD_ISSET(conn_sock, &readset)) {
+				printf("read from conection 1\n");
+				if (writefile(conn_sock, fp) != 0) {
+					break;
+				}
+			}
+			if (FD_ISSET(conn_sock_2, &readset)){
+				printf("read from conection 2\n");
+				if (writefile(conn_sock_2, fp) != 0) {
+					break;
+				}
+			}
+
 		}
-
-
-		// Create the file 
-		FILE *fp = fopen(filename, "wb");
-		if (fp == NULL) {
-			error_exit("Cannot open the file");
-		}
-
-		// Write data in to file 
-		writefile(conn_sock, fp);
+		
 		puts("Receive successfully!\n");
 
-		// Finish 
-		fclose(fp);
+		//fclose(fp);
 		close(conn_sock);
-
-		/*
-		for (;;) {
-			char buffer[1024];
-			// Each recv() will read only one individual message.
-			// Datagrams, not a stream!
-			int ret = recv(conn_sock, buffer, sizeof(buffer), 0);
-			if (ret > 0)
-				printf("Received: %s\n", buffer);
-			else
-				break;
-
-		}
-		*/
+		close(conn_sock_2);
 	}
+
+
 }
 
-void writefile(int sock_fd, FILE *fp) {
+int writefile(int sock_fd, FILE *fp) {
 	ssize_t n;
 	char buffer[BUFFER_SIZE];
 	bzero(buffer, BUFFER_SIZE);
 
-	while((n = recv(sock_fd, buffer, BUFFER_SIZE, 0)) > 0) {
+	n = recv(sock_fd, buffer, BUFFER_SIZE, 0);
+	if (n == 0) {
+		printf("connection %d closed\n", sock_fd);
+		return 1;
+	} else if (n > 0) {
+		printf("recv %ld\n", n);
+		/*
 		if(fwrite(buffer, sizeof(char), n, fp) < n) {
 			perror("Write File Error");
 			exit(1);
 		}
+		*/
 		bzero(buffer, BUFFER_SIZE); // Clear the buffer
+	} else {
+		perror("recv");
+		exit(1);
 	}
-
+	return 0;
 }
